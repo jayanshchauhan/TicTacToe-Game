d0ff993d986491b0369526acd07edcb430f38b42
Jayansh Chauhan <jayanshchauhan2001@gmail.com>
2022-02-16 12:28:52 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tic-Tac-Toe</title>
+        <link rel="stylesheet" href="indexx.css">
+    </head>
+    <body>
+        <div class="main-form">
+            <h1>Welcome to the world of TIC TAC TOE Game:</h1>
+
+         <!--   <form action="login.php" method="post">
+                <button type="login">Login </button>
+            </form>
+            
+            <form action="registration.php" method="post">
+                <button type="register">Register</button>
+            </form>
+-->
+<form method="post">
+<button name ="login" type="login">Login </button>
+<?php
+if(isset($_POST["login"])){
+header("location:login.php");
+}
+?>
+<button name="registration" type="register">Register </button>
+<?php
+if(isset($_POST["registration"])){
+header("location:registration.php");
+}
+?>
+</form>
+        </div>
+    </body>
+</html>
